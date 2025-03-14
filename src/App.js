393d6ac9fb583ac54dfbf1b47b97534bd6c0d44f
varikuppalavinod <varikuppalavinod258@gmail.com>
2025-03-14 22:09:25 +0530

    practice 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import React,{useState} from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App=()=>{
+  let arr=[{id:1,name:"vinod",village:"chinthakuntla",city:"kmp"},
+    {id:2,name:"ram",village:"chinthak",city:"kmp"},
+    {id:3,name:"ravi",village:"ap",city:"kmp"},
+    {id:4,name:"arun",village:"ts",city:"dvk"},
+    {id:5,name:"raju",village:"dvk",city:"nlg"},
+    {id:6,name:"anil",village:"ngm",city:"rmp"},
+    {id:7,name:"harish",village:"chk",city:"rmp"},
+    {id:8,name:"akul",village:"pervala",city:"kmp"},
+    {id:9,name:"ajay",village:"chinthakuntla",city:"kkk"}]
+
+    const[data,setdata]=useState(arr)
+
+    const deletehandler=(comingid)=>{
+      console.log(comingid)
+      const filterdata=data.filter((eachitem)=>{
+        return eachitem.id!==comingid
+      })
+      setdata(filterdata)
+    }
+return(
+  <div>
+  <div>welcome to react</div>
+  <ul className="container">
+    {data.map((eachitem)=>{
+      const{id,name,village,city}=eachitem
+      return<li key={id} className="libox">
+        <div>My name is :{name}</div>
+        <div>My village name is:{village}</div>
+        <div>My city name is :{city}</div>
+        <button onClick={(()=>deletehandler(id))}>Delete</button>
+      </li>
+    })}
+  </ul>
+
+  </div>
+)
 }
-
-export default App;
+export default App
