9efdc2f3779555d52ee90acd3a7912ec6f6c545b
varikuppalavinod <varikuppalavinod258@gmail.com>
2025-03-15 20:55:37 +0530

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,42 +1,20 @@
-import "./App.css"
 import React,{useState} from "react"
 
 const App=()=>{
-  let arr=[{id:1,name:"vinod",village:"chinthakuntla",city:"kmp"},
-    {id:2,name:"ram",village:"chinthak",city:"kmp"},
-    {id:3,name:"ravi",village:"ap",city:"kmp"},
-    {id:4,name:"arun",village:"ts",city:"dvk"},
-    {id:5,name:"raju",village:"dvk",city:"nlg"},
-    {id:6,name:"anil",village:"ngm",city:"rmp"},
-    {id:7,name:"harish",village:"chk",city:"rmp"},
-    {id:8,name:"akul",village:"pervala",city:"kmp"},
-    {id:9,name:"ajay",village:"chinthakuntla",city:"kkk"}]
 
-    const[data,setdata]=useState(arr)
-
-    const deletehandler=(comingid)=>{
-      console.log(comingid)
-      const filterdata=data.filter((eachitem)=>{
-        return eachitem.id!==comingid
-      })
-      setdata(filterdata)
-    }
-return(
-  <div>
-  <div>welcome to react</div>
-  <ul className="container">
-    {data.map((eachitem)=>{
-      const{id,name,village,city}=eachitem
-      return<li key={id} className="libox">
-        <div>My name is :{name}</div>
-        <div>My village name is:{village}</div>
-        <div>My city name is :{city}</div>
-        <button onClick={(()=>deletehandler(id))}>Delete</button>
-      </li>
-    })}
-  </ul>
-
-  </div>
-)
+  const[data,setdata]=useState(true)
+  const clickhandler=()=>{
+    setdata(!data)
+  }
+  return(
+    <div style={{textAlign:"center",fontSize:"20px"}}>
+      <button onClick={clickhandler}>{data?"hide":"show"}</button>
+    {data&&<p>Here's an example of a paragraph: As the sun began to set over the horizon,<br></br>
+       casting long shadows across the bustling streets of Bengaluru, a sense of <br/>
+       tranquility washed over the city. The vibrant energy of the day gradually <br/>
+        subsided, replaced by the soft glow of streetlights and the gentle hum of
+         traffic. </p>}
+    </div>
+  )
 }
 export default App
